1c8a942430ae3feb5c7cdf518cd015bb73dec687
Fernando <feralegre777@hotmail.com>
2021-06-12 18:23:15 -0300

    Subiendo

---
+++ 7. Sumar/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPos = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtCeros = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNeg = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSumaPos = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSumNeg = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCantPares = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.txtCantImp = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(125, 38)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 173)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(411, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Cantidad de Positivos"
+        '
+        'txtPos
+        '
+        Me.txtPos.Location = New System.Drawing.Point(531, 35)
+        Me.txtPos.Name = "txtPos"
+        Me.txtPos.Size = New System.Drawing.Size(100, 20)
+        Me.txtPos.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(300, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Procesar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtCeros
+        '
+        Me.txtCeros.Location = New System.Drawing.Point(531, 61)
+        Me.txtCeros.Name = "txtCeros"
+        Me.txtCeros.Size = New System.Drawing.Size(100, 20)
+        Me.txtCeros.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(426, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Cantidad de Ceros"
+        '
+        'txtNeg
+        '
+        Me.txtNeg.Location = New System.Drawing.Point(531, 87)
+        Me.txtNeg.Name = "txtNeg"
+        Me.txtNeg.Size = New System.Drawing.Size(100, 20)
+        Me.txtNeg.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(405, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Cantidad de Negativos"
+        '
+        'txtSumaPos
+        '
+        Me.txtSumaPos.Location = New System.Drawing.Point(531, 113)
+        Me.txtSumaPos.Name = "txtSumaPos"
+        Me.txtSumaPos.Size = New System.Drawing.Size(100, 20)
+        Me.txtSumaPos.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(427, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Suma de positivos"
+        '
+        'txtSumNeg
+        '
+        Me.txtSumNeg.Location = New System.Drawing.Point(531, 139)
+        Me.txtSumNeg.Name = "txtSumNeg"
+        Me.txtSumNeg.Size = New System.Drawing.Size(100, 20)
+        Me.txtSumNeg.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(420, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Suma de Negativos"
+        '
+        'txtCantPares
+        '
+        Me.txtCantPares.Location = New System.Drawing.Point(531, 165)
+        Me.txtCantPares.Name = "txtCantPares"
+        Me.txtCantPares.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantPares.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(426, 168)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(94, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Cantidad de Pares"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(651, 38)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Nuevo"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(651, 88)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 16
+        Me.Button4.Text = "Salir"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txtCantImp
+        '
+        Me.txtCantImp.Location = New System.Drawing.Point(531, 191)
+        Me.txtCantImp.Name = "txtCantImp"
+        Me.txtCantImp.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantImp.TabIndex = 18
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(416, 194)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(104, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Cantidad de Impares"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(822, 245)
+        Me.Controls.Add(Me.txtCantImp)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.txtCantPares)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtSumNeg)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSumaPos)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNeg)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCeros)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtPos)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPos As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txtCeros As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNeg As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtSumaPos As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtSumNeg As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCantPares As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents txtCantImp As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ 7. Sumar/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+
+    Dim numero As Integer = 0
+    Dim CantNumPos As Integer = 0
+    Dim CantNumNeg As Integer = 0
+    Dim CantCeros As Integer = 0
+    Dim SumNumPos As Integer = 0
+    Dim SumaNumNeg As Integer = 0
+    Dim CantPares As Integer
+    Dim CantImpares As Integer
+    Dim resultado As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        For i = 1 To 100
+            If (Rnd() * 50 > i) Then
+                ListBox1.Items.Add(Str(Math.Round(Rnd() * 100, 0)))
+            Else
+                ListBox1.Items.Add(Str(-Math.Round(Rnd() * 100, 0)))
+            End If
+        Next i
+    End Sub
+
+   
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        For i As Integer = 0 To 99
+
+            numero = ListBox1.Items(i).ToString()
+            If (numero > 0) Then
+
+                CantNumPos += 1
+                SumNumPos = SumNumPos + numero
+            End If
+
+            If (numero < 0) Then
+                CantNumNeg += 1
+                SumaNumNeg = SumaNumNeg + numero
+            End If
+
+            If (numero = 0) Then
+                CantCeros += 1
+            End If
+
+
+            resultado = ListBox1.Items(i).ToString() Mod 2
+            If resultado = 0 Then
+                CantPares = CantPares + 1
+            Else
+                CantImpares = CantImpares + 1
+
+            End If
+
+        Next
+        txtPos.Text = CantNumPos
+        txtNeg.Text = CantNumNeg
+        txtCeros.Text = CantCeros
+        txtSumaPos.Text = SumNumPos
+        txtSumNeg.Text = SumaNumNeg
+        txtCantPares.Text = CantPares
+        txtCantImp.Text = CantImpares
+
+    End Sub
+
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        txtCeros.Text = ""
+        txtPos.Text = ""
+        txtNeg.Text = ""
+        txtSumaPos.Text = ""
+        txtSumNeg.Text = ""
+        txtCantPares.Text = ""
+        txtCantImp.Text = ""
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Me.Close()
+
+    End Sub
+End Class
